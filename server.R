995ffc21816e338589828685ed4ade8cca61712f
Anh Tran <trannhatanh89@gmail.com>
2021-03-11 12:22:32 -0600

    made pathology and download buttons into modules

--- server.R
+++ server.R
@@ -1,41 +1,65 @@
+pathologyServer <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    ## Selectize input
+    updateSelectizeInput(
+      session, "gene", 
+      choices = HPAanalyze::hpa_histology_data$pathology$gene,
+      server = TRUE)
+    
+    updateSelectizeInput(
+      session, "cancer", 
+      choices = HPAanalyze::hpa_histology_data$pathology$cancer,
+      server = TRUE)
+    
+    ## Creating plot
+    output$plot <- renderPlot({
+      req(input$go, isolate(input$gene))
+      hpaVisPatho(targetGene = isolate(input$gene), 
+                  targetCancer = isolate(input$cancer))
+    })
+    
+    ## Creating table
+    data <- reactive({
+      req(input$go, isolate(input$gene))
+      hpaSubset(targetGene = isolate(input$gene), 
+                targetCancer = isolate(input$cancer))$pathology
+    })
+    
+    output$table <- renderDataTable(
+      data(),
+      options = list(pageLength = 5))
+    
+    output$download_data <- downloadHandler(
+      filename = function() {
+        paste0("pathology_", gsub("[^0-9]", '_', Sys.time()), ".csv")
+      },
+      content = function(file) {
+        vroom::vroom_write(data(), file, delim = ",")
+      }
+    )
+  })
+}
+
 server <- function(input, output, session) {
   
-  ## Selectize input
-  updateSelectizeInput(
-    session, "gene", 
-    choices = HPAanalyze::hpa_histology_data$pathology$gene,
-    server = TRUE)
+  # ## Selectize input
+  # updateSelectizeInput(
+  #   session, "gene", 
+  #   choices = HPAanalyze::hpa_histology_data$normal_tissue$gene,
+  #   server = TRUE)
+  # 
+  # updateSelectizeInput(
+  #   session, "tissue", 
+  #   choices = HPAanalyze::hpa_histology_data$normal_tissue$tissue,
+  #   server = TRUE)
+  # 
+  # updateSelectizeInput(
+  #   session, "cell_type", 
+  #   choices = HPAanalyze::hpa_histology_data$normal_tissue$cell_type,
+  #   server = TRUE)
   
-  updateSelectizeInput(
-    session, "cancer", 
-    choices = HPAanalyze::hpa_histology_data$pathology$cancer,
-    server = TRUE)
-  
-  ## Creating plot
-  output$plot <- renderPlot({
-    req(input$go, isolate(input$gene))
-    hpaVisPatho(targetGene = isolate(input$gene), 
-                targetCancer = isolate(input$cancer))
-    })
-  
-  ## Creating table
-  data <- reactive({
-    req(input$go, isolate(input$gene))
-    hpaSubset(targetGene = isolate(input$gene), 
-              targetCancer = isolate(input$cancer))$pathology
-    })
-  
-  output$table <- renderDataTable(
-    data(),
-    options = list(pageLength = 5))
-  
-  output$download_data <- downloadHandler(
-    filename = function() {
-      paste0("pathology_", gsub("[^0-9]", '_', Sys.time()), ".csv")
-    },
-    content = function(file) {
-      vroom::vroom_write(data(), file, delim = ",")
-    }
-  )
+## PATHOLOGY ===================================================================
+  pathologyServer("pathology")
+
   
 }
--- ui.R
+++ ui.R
@@ -1,27 +1,55 @@
+download_buttons_ui <- function(id) {
+  tagList(
+    HTML("<br/><br/>"),
+    downloadButton(NS(id,"download_data"), "Download data")
+  )
+}
+
+pathologyUI <- function(id) {
+  tagList(
+    sidebarLayout(
+      sidebarPanel(
+        selectInput(NS(id,"gene"), label = "Gene(s)", choices = NULL, multiple = TRUE),
+        selectInput(NS(id,"cancer"), label = HTML("Cancer(s) <br/> (Leave emplty for all)"), choices = NULL, multiple = TRUE),
+        actionButton(NS(id,"go"), "Go!", class = "btn-success"),
+        download_buttons_ui(id)
+      ),
+      mainPanel(
+        withSpinner(plotOutput(NS(id,"plot"), width = 500)),
+        dataTableOutput(NS(id,"table"))
+      )
+    )
+  )
+}
+
 ui <- navbarPage(
   
   theme = bslib::bs_theme(bootswatch = "flatly"),
   
   "HPAanalyze",
   
-  tabPanel("Tissue"),
+  tabPanel(
+    "Tissue",
+    # sidebarLayout(
+    #   sidebarPanel(
+    #     selectInput("gene", label = "Gene(s)", choices = NULL, multiple = TRUE),
+    #     selectInput("tissue", label = HTML("Tissue(s)"), choices = NULL, multiple = TRUE),
+    #     selectInput("cell_type", label = HTML("Cell type(s) <br/> (Leave emplty for all)"), choices = NULL, multiple = TRUE),
+    #     actionButton("go", "Go!", class = "btn-success"),
+    #     download_buttons_ui()
+    #   ),
+    #   mainPanel(
+    #     withSpinner(plotOutput("plot", width = 500)),
+    #     dataTableOutput("table")
+    #   )
+    # )
+  ),
   
   tabPanel(
     "Pathology",
-    sidebarLayout(
-      sidebarPanel(
-        selectInput("gene", label = "Gene(s)", choices = NULL, multiple = TRUE),
-        selectInput("cancer", label = HTML("Cancer(s) <br/> (Leave emplty for all)"), choices = NULL, multiple = TRUE),
-        actionButton("go", "Go!", class = "btn-success"),
-        HTML("<br/><br/>"),
-        downloadButton("download_data", "Download data")
-      ),
-      mainPanel(
-        withSpinner(plotOutput("plot", width = 500)),
-        dataTableOutput("table")
-      )
-    )
+    pathologyUI("pathology")
   ),
   
   tabPanel("Subcellular location")
 )
+
